de72af95edfc353b1ecbd005e9a6c369dc9a8013
phamhduyenduyen@gmail.com <phamhduyenduyen@gmail.com>
2026-06-08 20:12:57 +0700

    Implement loyalty customer discount feature

--- test_discount.py
+++ test_discount.py
@@ -1,6 +1,10 @@
 from discount import calculate_discount
+#TC01
 def test_vip_customer():
     assert calculate_discount(60000000) == 0.1
+#TC02
 def test_nomal_customer():
     assert calculate_discount(30000000) == 0
-    
+#TC03
+def test_reach_customer():
+    assert calculate_discount(49000000) == 0.1
